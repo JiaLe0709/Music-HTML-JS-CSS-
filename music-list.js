91ae36f83b7583a0c03047ea7e07e2f28e041522
Jia Le <88878287+JiaLe0709@users.noreply.github.com>
2022-09-12 21:16:20 +0800

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,20 @@
+let allMusic = [
+  {
+    name: "Thinking Out Loud",
+    artist: "Ed Sheeran",
+    img : "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Perfect",
+    artist: "Ed Sheeran",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Baby",
+    artist: "Justin Bieber & Ludacris",
+    img: "music-3",
+    src: "music-3"
+  },
+];
